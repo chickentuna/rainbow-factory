215ed0c189f27f9fd68b364a97ca0d44fe1bd1c8
Julien Poulton <julien.poulton@gmail.com>
2018-11-18 12:50:14 +0100

    include lib

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const WIDTH = 974
 export const HEIGHT = 548
+export const MINI_CUBE_SIZE = 6
 export const CUBE_SIZE = 48 // in pixels per side
 // factors of 48: 1, 2, 3, 4, 6, 8, 12, 16, 24
